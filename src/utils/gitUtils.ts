f9664dff954b7d4537d5fa60151ed6be0be8f34a
Bo.Jin <bo.jin@borgor.cn>
2024-09-29 11:55:59 +0800

    init a git repo

--- src/utils/gitUtils.ts
+++ src/utils/gitUtils.ts
@@ -1,5 +1,8 @@
 import clone from "git-clone";
 import { DEFAULT_BRANCH } from "@/constants/templates";
+import * as path from "node:path";
+import * as fs from "node:fs";
+import { exec } from "node:child_process";
 
 export class GitUtils {
   static async cloneGitRepo(
@@ -15,6 +18,23 @@ export class GitUtils {
       clone(repoUrl, projectPath, options, (err) => {
         if (err) {
           reject(err);
+        }
+        resolve(true);
+      });
+    });
+  }
+
+  static clearGitRepo(projectPath: string) {
+    if (fs.existsSync(path.join(projectPath, ".git"))) {
+      fs.rmSync(path.join(projectPath, ".git"), { recursive: true });
+    }
+  }
+
+  static async initGitRepo(projectPath: string) {
+    return new Promise((resolve, reject) => {
+      exec(`cd ${projectPath} && git init`, (error) => {
+        if (error) {
+          reject(error);
         }
         resolve(true);
       });
